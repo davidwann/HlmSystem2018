683b03caa240a38ec5b49e0f0f197e2fb42bc8f8
David Wann <david.wann@gmail.com>
2018-10-27 19:12:24 -0500

    new code

---
+++ HlmProject_0007/hlmWebApp/src/main/java/com/hlm/users/UserAccount.java
@@ -0,0 +1,56 @@
+package com.hlm.users;
+
+public class UserAccount {	
+	
+	private int id;
+	private String accountType;
+	private String zipCode;
+	private String[] interests;
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+	public String getAccountType() {
+		return this.accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+	public String getZipCode() {
+		return this.zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+	public String[] getInterests() {
+		return this.interests;
+	}
+
+	public void setInterests(String[] interests) {
+		this.interests = interests;
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+	
+	
+	
+	
+
+}
---
+++ HlmProject_0007/hlmWebApp/src/test/java/com/hlm/users/UserAccountTest.java
@@ -0,0 +1,45 @@
+package com.hlm.users;
+
+// import static org.junit.Assert.*;
+// import static org.junit.Assert.fail;
+// import static org.junit.Assert.fail;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class UserAccountTest {
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+	@Test
+	public void test() {
+		// fail("Not yet implemented");
+		Assert.fail("Not yet implemented");
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+	
+	@Test
+	public void test0002() {
+		
+		UserAccount ua = null;
+		
+		ua = new UserAccount();
+		
+		Assert.assertNotNull("ua (UserAccount should not be null!)", ua);
+	}
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+	
+
+	
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+	
+	
+
+	
+	/* - - - - - - - - - - - - - - - - - - - - - */
+
+}
